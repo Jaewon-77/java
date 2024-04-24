3d6dac4bb23f65fc17c0cfd270384cd8fd002f17
Jaewon-77 <babota0606@gmail.com>
2024-04-24 07:29:49 +0000

    java3

--- Sample.java
+++ Sample.java
@@ -1,15 +1,16 @@
-public class Sample {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-    }
-}
+// public class Sample {
+//     public static void main(String[] args) {
+//         System.out.println("HelloWorld");
+//     }
+// }
 
 public class Hero {
   private String name;
-  private int hp,mp;
+  private int hp, mp;
 
-  public Srting to String() {
+  @Override
+  public String toString() {
     return "勇者(名前=" + this.name
-    +"/HP" + this.hp + "/MP" + this.mp + ") ";
+      + "/HP=" + this.hp + "/MP=" + this.mp + ")";
   }
 }
