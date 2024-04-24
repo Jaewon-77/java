3e293c314fd527c8a27a97359085720688a43e36
Jaewon-77 <babota0606@gmail.com>
2024-04-24 07:27:50 +0000

    java2

--- Sample.java
+++ Sample.java
@@ -3,3 +3,13 @@ public class Sample {
         System.out.println("HelloWorld");
     }
 }
+
+public class Hero {
+  private String name;
+  private int hp,mp;
+
+  public Srting to String() {
+    return "勇者(名前=" + this.name
+    +"/HP" + this.hp + "/MP" + this.mp + ") ";
+  }
+}
