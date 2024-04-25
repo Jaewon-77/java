056e0a5a340f6e4d5a8419c6c5e742defc49393e
Jaewon-77 <babota0606@gmail.com>
2024-04-25 08:22:16 +0000

    java4

--- Sample.java
+++ Sample.java
@@ -4,13 +4,13 @@
 //     }
 // }
 
-public class Hero {
-  private String name;
-  private int hp, mp;
+// public class Hero {
+//   private String name;
+//   private int hp, mp;
 
-  @Override
-  public String toString() {
-    return "勇者(名前=" + this.name
-      + "/HP=" + this.hp + "/MP=" + this.mp + ")";
-  }
-}
+//   @Override
+//   public String toString() {
+//     return "勇者(名前=" + this.name
+//       + "/HP=" + this.hp + "/MP=" + this.mp + ")";
+//   }
+// }
