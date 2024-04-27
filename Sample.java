45ad8da0d2748dc7f91cf39e954bd806b38999ef
Jaewon-77 <babota0606@gmail.com>
2024-04-27 05:10:03 +0000

    java6

--- Sample.java
+++ Sample.java
@@ -1,16 +1,16 @@
-// public class Sample {
-//     public static void main(String[] args) {
-//         System.out.println("HelloWorld");
-//     }
-// }
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
 
-// public class Hero {
-//   private String name;
-//   private int hp, mp;
+public class Hero {
+  private String name;
+  private int hp, mp;
 
-//   @Override
-//   public String toString() {
-//     return "勇者(名前=" + this.name
-//       + "/HP=" + this.hp + "/MP=" + this.mp + ")";
-//   }
-// }
+  @Override
+  public String toString() {
+    return "勇者(名前=" + this.name
+      + "/HP=" + this.hp + "/MP=" + this.mp + ")";
+  }
+}
