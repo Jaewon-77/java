9f6b39ef5d3a8c7d59b6b7c577ee4a2a9961b753
Jaewon-77 <babota0606@gmail.com>
2024-06-10 12:15:16 +0000

    practice14

--- java.java
+++ java.java
@@ -10,7 +10,3 @@ public class HelloWorld {
     System.out.println("Hello,World!");
   }
 
-public class HelloWorld {
-  public static void main(String [] args) {
-    System.out.println("Hello,World!");
-  }
